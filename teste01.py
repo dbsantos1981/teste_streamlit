906a24afb3b84e9a1874fb65dd4ff8247d76b1bf
Daniel <33914193+dbsantos1981@users.noreply.github.com>
2022-12-21 17:54:06 -0300

    Update teste01.py

--- teste01.py
+++ teste01.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import spleeter as sp
-
-# from spleeter.separator import Separator
+from spleeter.separator import Separator
 
 st.title("Hello World")
-separator_lib = sp.separator.Separator("spleeter:2stems", multiprocess=False)
+separator_lib = Separator("spleeter:2stems", multiprocess=False)
 st.balloons()
